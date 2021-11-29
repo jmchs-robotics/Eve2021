846a6c1f8d036cc26c0e0191a853f6b2d930b147
Sydney63 <sm23059@judgememorial.com>
2021-11-29 15:26:10 -0700

    New Eve Path (straight to treasure chest, then to station)

--- src/main/java/org/usfirst/frc5933/Eve2019/commands/Autonomous_AIMSPaths.java
+++ src/main/java/org/usfirst/frc5933/Eve2019/commands/Autonomous_AIMSPaths.java
@@ -12,20 +12,18 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Autonomous_AIMSPaths extends CommandGroup {
 
-	/**in MIDDLE, Exchange Only, put jumper on slot 0 (GND to S)
-	 * on LEFT side of scale, put jumper on slot 1 (GND to S)
-	 * on RIGHT side of scale, put jumper on slot 2 (GND to S)
-	 * in MIDDLE, jumper in slot 5: no robots will block us
-	 * in MIDDLE, jumper in slot 3: go to right; other robot will block our left
-	 * in MIDDLE, jumper in slot 4: go to left; other robot will block our right
-	*/
+	
 	public Autonomous_AIMSPaths(String message) {
 		addSequential(new MoveArmToPos(ArmPosition.Start, false));
 
-		addSequential(new DriveStraightGyro(305, 1, true));
+		addSequential(new DriveStraightGyro(305, .99, true));
 
 		addSequential(new CloseTongs(false));
 
 		addSequential(new GyroTurn(-90, .5, .025));
+
+		addSequential(new OpenTongs(false));
+
+		addSequential(new DriveStraightGyro(230, .99, true));
 	}
 }
