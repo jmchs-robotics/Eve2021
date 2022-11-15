6f61103a45a748fcb209ff2575e83b0322d5c565
Ian Galaviz <ig25049@judgememorial.com>
2022-11-14 16:51:23 -0800

    + Fixed robot rotating issue - Goes too far - Drifts randomly

--- src/main/java/org/usfirst/frc5933/Eve2019/commands/Autonomous_AMESPaths.java
+++ src/main/java/org/usfirst/frc5933/Eve2019/commands/Autonomous_AMESPaths.java
@@ -2,7 +2,6 @@ package org.usfirst.frc5933.Eve2019.commands;
 
 import org.usfirst.frc5933.Eve2019.Robot;
 import org.usfirst.frc5933.Eve2019.SocketVisionSender;
-import org.usfirst.frc5933.Eve2019.subsystems.Arm.ArmPosition;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,23 +14,17 @@ public class Autonomous_AMESPaths extends CommandGroup {
 	
 	public Autonomous_AMESPaths() {
 
-		addSequential(new DriveStraightGyro(-135.5, -1, true)); 
+		addSequential(new DriveStraightGyro(135.5, 1, true)); 
+
+		//addSequential(new DriveStraightGyro(5, 1));
 		
 		//grab 
 
-		addSequential(new DriveStraightGyro(12, -1, true));
+		addSequential(new DriveStraightGyro(-12, -1, true));
 
 		addSequential(new GyroTurn(90, .5, .025));
 
-		addSequential(new DriveStraightGyro(-132.5, -1, true));
-
-		//addSequential(new CloseTongs(false));
-
-		//addSequential(new GyroTurn(180, .5, .025));
-
-		//addSequential(new OpenTongs(false));
-
-		//addSequential(new DriveStraightGyro(230, .99, true));
+		addSequential(new DriveStraightGyro(132.5, 1, true));
 	}
 }
 
